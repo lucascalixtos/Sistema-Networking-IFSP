0b1f93ba87665b43d4b8945547ac6b3ba34c395e
thiagosilva <thiagosilva196@gmail.com>
2020-10-29 23:00:56 -0300

    Criado viewcomponent do Painel

---
+++ PlataformaNetworking/ViewComponents/PainelViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlataformaNetworking.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlataformaNetworking.ViewComponents
+{
+
+
+
+    public class PainelViewComponent : ViewComponent {
+        private readonly PlataformaNetworkingContext _context;
+
+        public PainelViewComponent(PlataformaNetworkingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
