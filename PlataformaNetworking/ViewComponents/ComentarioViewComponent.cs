e28ede2e00f79e445a00340810cb35b4d13c523f
thiagosilva <thiagosilva196@gmail.com>
2020-11-25 21:43:10 -0300

    Correçao nos comentarios

--- PlataformaNetworking/ViewComponents/ComentarioViewComponent.cs
+++ PlataformaNetworking/ViewComponents/ComentarioViewComponent.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PlataformaNetworking.Data;
+using Remotion.Linq.Clauses;
 
 namespace PlataformaNetworking.ViewComponents
 {
@@ -15,9 +18,10 @@ namespace PlataformaNetworking.ViewComponents
             _context = context;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(int IdPost)
         {
-            return View(await _context.Comment.ToListAsync());
+            var comentarios = await _context.Comment.Where(x => x.IdPost == IdPost).ToListAsync();
+            return View(comentarios);
         }
     }
 }
